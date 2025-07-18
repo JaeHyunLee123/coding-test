cfb7aac63d7685c61b5be5d5bc859db7f4de7c23
JaeHyun Lee <104538469+JaeHyunLee123@users.noreply.github.com>
2025-07-18 13:57:23 +0900

    Time: 82 ms (33.18%), Space: 64.4 MB (84.79%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
@@ -1,64 +1,64 @@
-class Maxheap{
-    constructor(){
-        this.heap = []
-    }
+// class Maxheap{
+//     constructor(){
+//         this.heap = []
+//     }
 
-    size(){
-        return this.heap.length;
-    }
+//     size(){
+//         return this.heap.length;
+//     }
 
-    heapifyUp(index){
-        const parent = Math.floor((index - 1) / 2);
+//     heapifyUp(index){
+//         const parent = Math.floor((index - 1) / 2);
 
-        if(index > 0 && this.heap[index] > this.heap[parent]){
-            const temp = this.heap[index];
-            this.heap[index] = this.heap[parent];
-            this.heap[parent] = temp;
+//         if(index > 0 && this.heap[index] > this.heap[parent]){
+//             const temp = this.heap[index];
+//             this.heap[index] = this.heap[parent];
+//             this.heap[parent] = temp;
 
-            this.heapifyUp(parent)
-        }
-    }
+//             this.heapifyUp(parent)
+//         }
+//     }
 
-    heapifyDown(index){
-        let max = index;
-        const left = index * 2 + 1;
-        const right = index * 2 + 2;
+//     heapifyDown(index){
+//         let max = index;
+//         const left = index * 2 + 1;
+//         const right = index * 2 + 2;
 
-        if(left < this.size() && this.heap[max] < this.heap[left]){
-            max = left;
-        }
+//         if(left < this.size() && this.heap[max] < this.heap[left]){
+//             max = left;
+//         }
 
-        if(right < this.size() && this.heap[max] < this.heap[right]){
-            max = right;
-        }
+//         if(right < this.size() && this.heap[max] < this.heap[right]){
+//             max = right;
+//         }
 
-        if(index !== max){
-            const temp = this.heap[index];
-            this.heap[index] = this.heap[max];
-            this.heap[max] = temp;
+//         if(index !== max){
+//             const temp = this.heap[index];
+//             this.heap[index] = this.heap[max];
+//             this.heap[max] = temp;
 
-            this.heapifyDown(max);
-        }
-    }
+//             this.heapifyDown(max);
+//         }
+//     }
 
-    push(num){
-        this.heap.push(num);
+//     push(num){
+//         this.heap.push(num);
 
-        this.heapifyUp(this.size() - 1);
-    }
+//         this.heapifyUp(this.size() - 1);
+//     }
 
-    pop(){
-        const root = this.heap[0];
+//     pop(){
+//         const root = this.heap[0];
 
-        const temp = this.heap.pop();
+//         const temp = this.heap.pop();
 
-        this.heap[0] = temp;
+//         this.heap[0] = temp;
 
-        this.heapifyDown(0);
+//         this.heapifyDown(0);
 
-        return root;
-    }
-}
+//         return root;
+//     }
+// }
 
 /**
  * @param {number[]} nums
@@ -66,17 +66,21 @@ class Maxheap{
  * @return {number}
  */
 var findKthLargest = function(nums, k) {
-    const pq = new Maxheap();
+    // const pq = new Maxheap();
 
-    nums.forEach(num => {
-        pq.push(num);
-    })
+    // nums.forEach(num => {
+    //     pq.push(num);
+    // })
 
-    let answer = 0;
+    // let answer = 0;
 
-    for(let i = 0; i < k; i++){
-        answer = pq.pop()
-    }
+    // for(let i = 0; i < k; i++){
+    //     answer = pq.pop()
+    // }
 
-    return answer
+    // return answer
+
+    nums.sort((a,b) => b - a);
+
+    return nums[k-1]
 };
