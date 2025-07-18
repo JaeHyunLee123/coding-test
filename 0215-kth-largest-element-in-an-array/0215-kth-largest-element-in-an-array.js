c72966cd6a90f1a66c92212d6cf3c333a5f67d0b
JaeHyun Lee <104538469+JaeHyunLee123@users.noreply.github.com>
2025-07-18 13:56:33 +0900

    Time: 53 ms (70.85%), Space: 67.2 MB (57.04%) - LeetHub

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
@@ -0,0 +1,82 @@
+class Maxheap{
+    constructor(){
+        this.heap = []
+    }
+
+    size(){
+        return this.heap.length;
+    }
+
+    heapifyUp(index){
+        const parent = Math.floor((index - 1) / 2);
+
+        if(index > 0 && this.heap[index] > this.heap[parent]){
+            const temp = this.heap[index];
+            this.heap[index] = this.heap[parent];
+            this.heap[parent] = temp;
+
+            this.heapifyUp(parent)
+        }
+    }
+
+    heapifyDown(index){
+        let max = index;
+        const left = index * 2 + 1;
+        const right = index * 2 + 2;
+
+        if(left < this.size() && this.heap[max] < this.heap[left]){
+            max = left;
+        }
+
+        if(right < this.size() && this.heap[max] < this.heap[right]){
+            max = right;
+        }
+
+        if(index !== max){
+            const temp = this.heap[index];
+            this.heap[index] = this.heap[max];
+            this.heap[max] = temp;
+
+            this.heapifyDown(max);
+        }
+    }
+
+    push(num){
+        this.heap.push(num);
+
+        this.heapifyUp(this.size() - 1);
+    }
+
+    pop(){
+        const root = this.heap[0];
+
+        const temp = this.heap.pop();
+
+        this.heap[0] = temp;
+
+        this.heapifyDown(0);
+
+        return root;
+    }
+}
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+    const pq = new Maxheap();
+
+    nums.forEach(num => {
+        pq.push(num);
+    })
+
+    let answer = 0;
+
+    for(let i = 0; i < k; i++){
+        answer = pq.pop()
+    }
+
+    return answer
+};
