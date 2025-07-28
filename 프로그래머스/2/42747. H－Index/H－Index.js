d7d32e0071b6741e47087213922263c3165d39c6
JaeHyun Lee <104538469+JaeHyunLee123@users.noreply.github.com>
2025-07-28 14:15:27 +0900

    [level 2] Title: H-Index, Time: 0.44 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -0,0 +1,18 @@
+function solution(citations) {
+    var answer = 0;
+    
+    citations.sort((a,b) => b - a);
+    
+    for(let i = 0; i < citations.length; i++){
+        const citation = citations[i]
+        
+        if(citation > i){
+            answer++;
+        }else{
+            break;
+        }
+    }
+
+    
+    return answer;
+}
